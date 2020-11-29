cc7fca780fcee7f16705bdc5da8e54a5bb19fd89
skkujyp <jongyeong83@gmail.com>
2020-11-29 21:51:02 +0900

    math change

--- report_card.c
+++ report_card.c
@@ -3,7 +3,7 @@
 void main(){
 	printf("This is Report Cards.c\n");
 	printf("what is the difference ? \n");
-	printf("Math: 0 \n");
+	printf("Math: 100 \n");
 	printf("English: 100 \n");
 	printf("Sum grade: 190 \n");
 	printf("Average grade: 95 \n");
